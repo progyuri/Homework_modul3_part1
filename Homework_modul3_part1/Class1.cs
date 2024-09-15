ce118fd8689f71507c6c647135c43a4998ed7975
bt-29 <bt-29@DESKTOP-E2AJF84>
2024-09-15 17:38:32 +0300

    Добавьте файлы проекта.

---
+++ Homework_modul3_part1/Class1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_modul3_part1
+{
+    class Website
+    {
+        private string name;
+        private string url;
+        private string description;
+        private string ipAddress;
+
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string IpAddress { get; set; }
+
+        // Конструктор класса
+        public Website(string name, string url, string description, string ipAddress)
+        {
+            this.name = name;
+            this.url = url;
+            this.description = description;
+            this.ipAddress = ipAddress;
+        }
+
+        // Метод для ввода данных
+        public void InputData()
+        {
+            Console.Write("Введите название сайта: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Введите путь к сайту (URL): ");
+            Url = Console.ReadLine();
+
+            Console.Write("Введите описание сайта: ");
+            Description = Console.ReadLine();
+
+            Console.Write("Введите IP адрес сайта: ");
+            IpAddress = Console.ReadLine();
+        }
+
+        // Метод для вывода данных
+        public void DisplayData()
+        {
+            Console.WriteLine("Название сайта: " + Name);
+            Console.WriteLine("Путь к сайту (URL): " + Url);
+            Console.WriteLine("Описание сайта: " + Description);
+            Console.WriteLine("IP адрес сайта: " + IpAddress);
+        }
+
+
+    }
+
+
+    class Journal
+    {
+        // Поля класса
+        private string title;
+        private int yearOfEstablishment;
+        private string description;
+        private string contactPhone;
+        private string contactEmail;
+
+        // Конструктор класса
+        public Journal(string title, int yearOfEstablishment, string description, string contactPhone, string contactEmail)
+        {
+            this.title = title;
+            this.yearOfEstablishment = yearOfEstablishment;
+            this.description = description;
+            this.contactPhone = contactPhone;
+            this.contactEmail = contactEmail;
+        }
+
+        // Методы для доступа к полям
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public void SetTitle(string value)
+        {
+            title = value;
+        }
+
+        public int GetYearOfEstablishment()
+        {
+            return yearOfEstablishment;
+        }
+
+        public void SetYearOfEstablishment(int value)
+        {
+            yearOfEstablishment = value;
+        }
+
+        public string GetDescription()
+        {
+            return description;
+        }
+
+        public void SetDescription(string value)
+        {
+            description = value;
+        }
+
+        public string GetContactPhone()
+        {
+            return contactPhone;
+        }
+
+        public void SetContactPhone(string value)
+        {
+            contactPhone = value;
+        }
+
+        public string GetContactEmail()
+        {
+            return contactEmail;
+        }
+
+        public void SetContactEmail(string value)
+        {
+            contactEmail = value;
+        }
+
+        // Метод для ввода данных
+        public void InputData()
+        {
+            Console.Write("Введите название журнала: ");
+            SetTitle(Console.ReadLine());
+
+            Console.Write("Введите год основания: ");
+            SetYearOfEstablishment(Convert.ToInt32(Console.ReadLine()));
+
+            Console.Write("Введите описание журнала: ");
+            SetDescription(Console.ReadLine());
+
+            Console.Write("Введите контактный телефон: ");
+            SetContactPhone(Console.ReadLine());
+
+            Console.Write("Введите контактный e-mail: ");
+            SetContactEmail(Console.ReadLine());
+        }
+
+        // Метод для вывода данных
+        public void DisplayData()
+        {
+            Console.WriteLine("Название журнала: " + GetTitle());
+            Console.WriteLine("Год основания: " + GetYearOfEstablishment());
+            Console.WriteLine("Описание журнала: " + GetDescription());
+            Console.WriteLine("Контактный телефон: " + GetContactPhone());
+            Console.WriteLine("Контактный e-mail: " + GetContactEmail());
+        }
+    }
+    class Shop
+    {
+        // Поля класса
+        private string _name;
+        private string _address;
+        private string _profileDescription;
+        private string _contactPhone;
+        private string _contactEmail;
+
+        // Методы для доступа к полям
+        public string GetName() {return _name; }
+
+        public void SetName(string value) {_name = value;}
+
+        public string GetAddress() { return _address; }
+
+
+        public void SetAddress(string value)
+        {
+            _address = value;
+        }
+
+        public string GetProfileDescription()
+        {
+            return _profileDescription;
+        }
+
+        public void SetProfileDescription(string value)
+        {
+            _profileDescription = value;
+        }
+
+        public string GetContactPhone()
+        {
+            return _contactPhone;
+        }
+
+        public void SetContactPhone(string value)
+        {
+            _contactPhone = value;
+        }
+
+        public string GetContactEmail()
+        {
+            return _contactEmail;
+        }
+
+        public void SetContactEmail(string value)
+        {
+            _contactEmail = value;
+        }
+
+        // Метод для ввода данных
+        public void InputData()
+        {
+            Console.Write("Введите название магазина: ");
+            SetName(Console.ReadLine());
+
+            Console.Write("Введите адрес магазина: ");
+            SetAddress(Console.ReadLine());
+
+            Console.Write("Введите описание профиля магазина: ");
+            SetProfileDescription(Console.ReadLine());
+
+            Console.Write("Введите контактный телефон: ");
+            SetContactPhone(Console.ReadLine());
+
+            Console.Write("Введите контактный e-mail: ");
+            SetContactEmail(Console.ReadLine());
+        }
+
+        // Метод для вывода данных
+        public void DisplayData()
+        {
+            Console.WriteLine("Название магазина: " + GetName());
+            Console.WriteLine("Адрес магазина: " + GetAddress());
+            Console.WriteLine("Описание профиля магазина: " + GetProfileDescription());
+            Console.WriteLine("Контактный телефон: " + GetContactPhone());
+            Console.WriteLine("Контактный e-mail: " + GetContactEmail());
+        }
+    }
+
+}
+
+
+
---
+++ Homework_modul3_part1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_modul3_part1
+{
+    internal class Program
+    {
+        //Method for display square
+        static void DisplaySquare(int sideLength, string symbol, bool isFilled = true)
+        {
+            for (int i = 0; i < sideLength; i++)
+            {
+                for (int j = 0; j < sideLength; j++)
+                {
+                    if (isFilled || i == 0 || i == sideLength - 1 || j == 0 || j == sideLength - 1)
+                    {
+                        Console.Write(symbol);
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        //Method for check palindrome
+        static bool IsPalindrome(int number)
+        {
+            // Конвертируем число в строку
+            string str = number.ToString();
+            int length = str.Length;
+
+            // Проверяем строку на палиндром
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (str[i] != str[length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //Method for filter array
+        static int[] FilterArray(int[] originalArray, int[] filterArray)
+        {
+            // Способ с использованием LINQ
+            //return originalArray.Where(x => !filterArray.Contains(x)).ToArray();
+
+            int[] resultArray = new int[originalArray.Length];
+            int count = 0;
+
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                if (Array.IndexOf(filterArray, originalArray[i]) == -1)
+                {
+                    resultArray[count++] = originalArray[i];
+                }
+            }
+
+            Array.Resize(ref resultArray, count);
+            return resultArray;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set side of square->");
+            int side = 0;
+            if (!int.TryParse(Console.ReadLine(), out side))
+            {
+                Console.WriteLine("Invalid input");
+            }
+            Console.WriteLine("Set symbol of square->");
+            string symbol = Console.ReadLine();
+
+            DisplaySquare(side,symbol);
+            Console.WriteLine();
+//---------------------------------------------------------------------------------------------------------------------------
+
+            // Пример использования метода для проверки палиндрома - IsPalindrome
+            int number = 12321;
+            Console.WriteLine($"{number} is palindrome: {IsPalindrome(number)}");
+            Console.WriteLine();
+
+//---------------------------------------------------------------------------------------------------------------------------
+ 
+            int[] originalArray = { 1, 2, 6, -1, 88, 7, 6 };
+            int[] filterArray = { 6, 88, 7 };
+
+            int[] filteredArray = FilterArray(originalArray, filterArray);
+
+            Console.WriteLine("Result filtered: " + string.Join(", ", filteredArray));
+
+            //----------------------------------------------------------------------------------------------------------------------------
+
+            Console.WriteLine();
+
+            // Создание объекта с использованием конструктора
+            Website website = new Website("Пример сайта", "https://example.com", "Описание примера", "192.168.0.1");
+
+
+
+            // Ввод данных
+            website.InputData();
+
+            // Вывод данных
+            website.DisplayData();
+
+            // Пример использования свойств
+            Console.WriteLine("Название сайта через свойство: " + website.Name);
+            website.Name = "Новое название сайта";
+            Console.WriteLine("Обновленное название сайта через свойство: " + website.Name);
+
+
+            //Аналогично использование классов Journal и Shop
+        }
+    }
+}
